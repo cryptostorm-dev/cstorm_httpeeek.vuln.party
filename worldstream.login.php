9edcc5068773d9717ae59f4de20cb7eb7a5413ec
Pattern Juggled ie ðørkßöt <pj@cryptostorm.is>
2015-02-26 15:15:46 -0500

    generates enormous background activity...
    source: https://www.worldstream.nl/login.php

---
+++ worldstream.login.php
@@ -0,0 +1,170 @@
+view-source:https://www.worldstream.nl/login.php
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/.php">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="description" content="" />
+<meta name="keywords" content="" />
+<meta name="author" content="Eyeron Media, (c) Eyeron Media. All Rights Reserved." />
+<meta name="robots" content="index, follow" />
+<meta name="revisit-after" content="7 days" />
+<title>Worldstream Internet Services</title>
+<link rel="icon" href="favicon.ico" type="image/x-icon" />
+<link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
+<link rel="stylesheet" type="text/css" href="style/ws.css" />
+<script src="js/jquery-1.8.3.min.js" type="text/javascript"></script>
+<script src="js/jquery.pngFix.pack.js" type="text/javascript"></script>
+<script src="js/cufon-yui.js" type="text/javascript"></script>
+<script src="js/ws.js" type="text/javascript"></script>
+<script src="js/js.js" type="text/javascript"></script>
+<script type="text/javascript">
+        Cufon.replace('h1, h2, #nav, .product, .payoff_low, #footer_content, #copyright, #title_sub, .pricetag, .title, #lang, #deal', {hover: true});
+	$(document).ready(function(){ 
+    $(document).pngFix(); 
+});
+</script>
+</script>
+<script type="text/JavaScript" src="/livehelp/scripts/jquery-latest.js"></script>
+<script type="text/JavaScript" src="https://www.worldstream.nl/livehelp/scripts/jquery-latest.js"></script>
+<script type="text/javascript">
+<!--
+    var LiveHelpSettings = {};
+    LiveHelpSettings.server = 'www.worldstream.nl';
+    LiveHelpSettings.embedded = true;
+    (function($) {
+        // JavaScript
+        LiveHelpSettings.server = LiveHelpSettings.server.replace(/[a-z][a-z0-9+\-.]*:\/\/|\/livehelp\/*(\/|[a-z0-9\-._~%!$&'()*+,;=:@\/]*(?![a-z0-9\-._~%!$&'()*+,;=:@]))|\/*$/g, '');
+        var LiveHelp = document.createElement('script'); LiveHelp.type = 'text/javascript'; LiveHelp.async = true;
+        LiveHelp.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + LiveHelpSettings.server + '/livehelp/scripts/jquery.livehelp.min.js';
+        var s = document.getElementsByTagName('script')[0];
+        s.parentNode.insertBefore(LiveHelp, s);
+    })(jQuery);
+-->
+</script>
+<script type="text/javascript" src="js/jquery.floatobject-1.0.js"></script>
+<script type="text/javascript" src="js/step-2.js"></script>
+</head>
+<body class="custom">
+<div id="holder">
+  <div id="header">
+    <div id="logo"><a href="index.php"><img src="/images/worldstream_logo.jpg" width="206" height="54" alt="Worldstream" /></a></div>
+    <div id="lang"><a href="/login.php?lang=en">English</a>&nbsp;|&nbsp;<a href="/login.php?lang=nl">Nederlands</a></div>
+    <div id="nav">
+      <ul>
+        <li class="item2"><a href="/contact.php">Contact</a></li>
+        <li class="item"><a href="/about.php">About us</a></li>
+        <li class="item"><a href="/colocation.php">Colocation</a></li>
+        <li class="item"><a href="/reseller.php">Reseller</a></li>
+        <li class="item"><a href="/lease-to-own.php">Lease-to-Own</a></li>
+        <li class="item"><a href="/unmetered.php">Unmetered</a></li>
+        <li class="item"><a href="/special.php">Specials</a></li>
+        <li class="item"><a href="/custom_dedicated_servers.php">Custom dedicated</a></li>
+        <li class="item"><a href="/premium_dedicated_servers.php">Premium dedicated</a></li>
+      </ul>
+    </div>
+  </div>
+  <div id="title_sub"></div>
+  <div class="payoff_low"><span class="blue">Login</span> with your account or  <span class="blue">register</span></div>
+  <div class="content">
+    <div class="column">
+      <div><h1 class="select">Login</h1></div>
+      <div class="package_select">
+                                      <table width="95%" border="0" cellspacing="0" cellpadding="0">
+	  <form action="https://www.worldstream.nl/login.php" method="post" name="register" id="register">
+          <input type="hidden" name="login" value="1">
+          <tr>
+            <td>E-mail address</td><td align="right"><input type="text" name="username"></td>
+	  </tr>
+	  <tr>
+            <td>Password</td><td align="right"><input type="password" name="password"></td>
+	  </tr>
+	  <tr>
+	    <td>&nbsp;</td>
+	  </tr>
+	  <tr>
+            <td>&nbsp;</td><td><input name="submitButton" type="image" title="Login" src="images/login.png" /></td>
+	  </tr>
+	</form>
+        </table>
+      </div>
+    </div>
+    <div class="column">
+      <div><h1 class="select">Register</h1></div>
+      <div class="package_select">
+        <table width="95%" border="0" cellspacing="0" cellpadding="0">
+	<form action="https://www.worldstream.nl/login.php" method="post" name="register" id="register">
+	<input type="hidden" name="register" value="1">
+          <tr>
+            <td>Company name</td><td><input type="text" name="companyname" id="companyname" maxlength="50" size="22" value=""></td>
+	  </tr>
+	              <tr>
+            <td>Gender *</td><td><select name="gender" id="gender"><option value="Mr">Mr.</option><option value="Ms">Ms.</option></select></td>
+	  </tr>
+	              <tr>
+            <td>First name *</td><td><input type="text" name="firstname" id="firstname" maxlength="50" size="22" value=""></td>
+	  </tr>
+	              <tr>
+            <td>Last name *</td><td><input type="text" name="lastname" id="lastname" maxlength="50" size="22" value=""></td>
+	  </tr>
+	              <tr>
+            <td>Street name *</td><td><input type="text" name="streetname" id="streetname" maxlength="50" size="22" value=""></td>
+	  </tr>
+	              <tr>
+            <td>Number *</td><td><input type="text" name="housenumber" id="housenumber" maxlength="10" size="4" value=""></td>
+	  </tr>
+	              <tr>
+            <td>Postal code *</td><td><input type="text" name="postalcode" id="postalcode" maxlength="20" size="10" value=""></td>
+	  </tr>
+	    	  <tr>
+	    <td>City *</td><td><input type="text" name="city" id="city" maxlength="50" size="22" value=""></td>
+	  </tr>
+	              <tr>
+            <td>Country *</td><td><select name="country" style="width:165px">
+        <option value="AF">Afghanistan</option><option value="AL">Albania</option><option value="DZ">Algeria</option><option value="AS">American Samoa</option><option value="AD">Andorra</option><option value="AO">Angola</option><option value="AI">Anguilla</option><option value="AQ">Antarctica</option><option value="AG">Antigua and Barbuda</option><option value="AR">Argentina</option><option value="AM">Armenia</option><option value="AW">Aruba</option><option value="AU">Australia</option><option value="AT">Austria</option><option value="AZ">Azerbaijan</option><option value="BS">Bahamas</option><option value="BH">Bahrain</option><option value="BD">Bangladesh</option><option value="BB">Barbados</option><option value="BY">Belarus</option><option value="BE">Belgium</option><option value="BZ">Belize</option><option value="BJ">Benin</option><option value="BM">Bermuda</option><option value="BT">Bhutan</option><option value="BO">Bolivia</option><option value="BA">Bosnia and Herzegovina</option><option value="BW">Botswana</option><option value="BV">Bouvet Island</option><option value="BR">Brazil</option><option value="IO">British Indian Ocean Territory</option><option value="BN">Brunei Darussalam</option><option value="BG">Bulgaria</option><option value="BF">Burkina Faso</option><option value="BI">Burundi</option><option value="KH">Cambodia</option><option value="CM">Cameroon</option><option value="CA">Canada</option><option value="CV">Cape Verde</option><option value="KY">Cayman Islands</option><option value="CF">Central African Republic</option><option value="TD">Chad</option><option value="CL">Chile</option><option value="CN">China</option><option value="CX">Christmas Island</option><option value="CC">Cocos (Keeling) Islands</option><option value="CO">Colombia</option><option value="KM">Comoros</option><option value="CG">Congo</option><option value="CD">Congo, the Democratic Republic of the</option><option value="CK">Cook Islands</option><option value="CR">Costa Rica</option><option value="CI">Cote D'Ivoire</option><option value="HR">Croatia</option><option value="CU">Cuba</option><option value="CY">Cyprus</option><option value="CZ">Czech Republic</option><option value="DK">Denmark</option><option value="DJ">Djibouti</option><option value="DM">Dominica</option><option value="DO">Dominican Republic</option><option value="EC">Ecuador</option><option value="EG">Egypt</option><option value="SV">El Salvador</option><option value="GQ">Equatorial Guinea</option><option value="ER">Eritrea</option><option value="EE">Estonia</option><option value="ET">Ethiopia</option><option value="FK">Falkland Islands (Malvinas)</option><option value="FO">Faroe Islands</option><option value="FJ">Fiji</option><option value="FI">Finland</option><option value="FR">France</option><option value="GF">French Guiana</option><option value="PF">French Polynesia</option><option value="TF">French Southern Territories</option><option value="GA">Gabon</option><option value="GM">Gambia</option><option value="GE">Georgia</option><option value="DE">Germany</option><option value="GH">Ghana</option><option value="GI">Gibraltar</option><option value="GR">Greece</option><option value="GL">Greenland</option><option value="GD">Grenada</option><option value="GP">Guadeloupe</option><option value="GU">Guam</option><option value="GT">Guatemala</option><option value="GN">Guinea</option><option value="GW">Guinea-Bissau</option><option value="GY">Guyana</option><option value="HT">Haiti</option><option value="HM">Heard Island and Mcdonald Islands</option><option value="VA">Holy See (Vatican City State)</option><option value="HN">Honduras</option><option value="HK">Hong Kong</option><option value="HU">Hungary</option><option value="IS">Iceland</option><option value="IN">India</option><option value="ID">Indonesia</option><option value="IR">Iran, Islamic Republic of</option><option value="IQ">Iraq</option><option value="IE">Ireland</option><option value="IL">Israel</option><option value="IT">Italy</option><option value="JM">Jamaica</option><option value="JP">Japan</option><option value="JO">Jordan</option><option value="KZ">Kazakhstan</option><option value="KE">Kenya</option><option value="KI">Kiribati</option><option value="KP">Korea, Democratic People's Republic of</option><option value="KR">Korea, Republic of</option><option value="KW">Kuwait</option><option value="KG">Kyrgyzstan</option><option value="LA">Lao People's Democratic Republic</option><option value="LV">Latvia</option><option value="LB">Lebanon</option><option value="LS">Lesotho</option><option value="LR">Liberia</option><option value="LY">Libyan Arab Jamahiriya</option><option value="LI">Liechtenstein</option><option value="LT">Lithuania</option><option value="LU">Luxembourg</option><option value="MO">Macao</option><option value="MK">Macedonia, the Former Yugoslav Republic of</option><option value="MG">Madagascar</option><option value="MW">Malawi</option><option value="MY">Malaysia</option><option value="MV">Maldives</option><option value="ML">Mali</option><option value="MT">Malta</option><option value="MH">Marshall Islands</option><option value="MQ">Martinique</option><option value="MR">Mauritania</option><option value="MU">Mauritius</option><option value="YT">Mayotte</option><option value="MX">Mexico</option><option value="FM">Micronesia, Federated States of</option><option value="MD">Moldova, Republic of</option><option value="MC">Monaco</option><option value="MN">Mongolia</option><option value="MS">Montserrat</option><option value="MA">Morocco</option><option value="MZ">Mozambique</option><option value="MM">Myanmar</option><option value="NA">Namibia</option><option value="NR">Nauru</option><option value="NP">Nepal</option><option value="NL">Netherlands</option><option value="AN">Netherlands Antilles</option><option value="NC">New Caledonia</option><option value="NZ">New Zealand</option><option value="NI">Nicaragua</option><option value="NE">Niger</option><option value="NG">Nigeria</option><option value="NU">Niue</option><option value="NF">Norfolk Island</option><option value="MP">Northern Mariana Islands</option><option value="NO">Norway</option><option value="OM">Oman</option><option value="PK">Pakistan</option><option value="PW">Palau</option><option value="PS">Palestinian Territory, Occupied</option><option value="PA">Panama</option><option value="PG">Papua New Guinea</option><option value="PY">Paraguay</option><option value="PE">Peru</option><option value="PH">Philippines</option><option value="PN">Pitcairn</option><option value="PL">Poland</option><option value="PT">Portugal</option><option value="PR">Puerto Rico</option><option value="QA">Qatar</option><option value="RE">Reunion</option><option value="RO">Romania</option><option value="RU">Russian Federation</option><option value="RW">Rwanda</option><option value="SH">Saint Helena</option><option value="KN">Saint Kitts and Nevis</option><option value="LC">Saint Lucia</option><option value="PM">Saint Pierre and Miquelon</option><option value="VC">Saint Vincent and the Grenadines</option><option value="WS">Samoa</option><option value="SM">San Marino</option><option value="ST">Sao Tome and Principe</option><option value="SA">Saudi Arabia</option><option value="SN">Senegal</option><option value="CS">Serbia and Montenegro</option><option value="SC">Seychelles</option><option value="SL">Sierra Leone</option><option value="SG">Singapore</option><option value="SK">Slovakia</option><option value="SI">Slovenia</option><option value="SB">Solomon Islands</option><option value="SO">Somalia</option><option value="ZA">South Africa</option><option value="GS">South Georgia and the South Sandwich Islands</option><option value="ES">Spain</option><option value="LK">Sri Lanka</option><option value="SD">Sudan</option><option value="SR">Suriname</option><option value="SJ">Svalbard and Jan Mayen</option><option value="SZ">Swaziland</option><option value="SE">Sweden</option><option value="CH">Switzerland</option><option value="SY">Syrian Arab Republic</option><option value="TW">Taiwan, Province of China</option><option value="TJ">Tajikistan</option><option value="TZ">Tanzania, United Republic of</option><option value="TH">Thailand</option><option value="TL">Timor-Leste</option><option value="TG">Togo</option><option value="TK">Tokelau</option><option value="TO">Tonga</option><option value="TT">Trinidad and Tobago</option><option value="TN">Tunisia</option><option value="TR">Turkey</option><option value="TM">Turkmenistan</option><option value="TC">Turks and Caicos Islands</option><option value="TV">Tuvalu</option><option value="UG">Uganda</option><option value="UA">Ukraine</option><option value="AE">United Arab Emirates</option><option value="GB">United Kingdom</option><option value="US">United States</option><option value="UM">United States Minor Outlying Islands</option><option value="UY">Uruguay</option><option value="UZ">Uzbekistan</option><option value="VU">Vanuatu</option><option value="VE">Venezuela</option><option value="VN">Viet Nam</option><option value="VG">Virgin Islands, British</option><option value="VI">Virgin Islands, U.s.</option><option value="WF">Wallis and Futuna</option><option value="EH">Western Sahara</option><option value="YE">Yemen</option><option value="ZM">Zambia</option><option value="ZW">Zimbabwe</option></td>
+	  </tr>
+	    	  <tr>
+	    <td>Telephone number *</td><td><input type="text" name="telephone" id="telephone" maxlength="50" size="22" value=""></td>
+	  </tr>
+	              <tr>
+            <td>E-mail address *</td><td><input type="text" name="email" id="email" maxlength="100" size="22" value=""></td>
+	  </tr>
+	              <tr>
+            <td>E-mail address again *</td><td><input type="text" name="email2" id="email2" maxlength="100" size="22" value=""></td>
+	  </tr>
+	    	  <tr>
+            <td>Password *</td><td><input type="password" size="22" name="password"></td>
+	  </tr>
+	    	  <tr>
+            <td>Password again *</td><td><input type="password" size="22" name="password2"></td>
+	  </tr>
+	    	  <tr>
+            <td>I agree with the <a href="terms.php">terms</a> of service *</td><td><input type="checkbox" name="agree"></td>
+	  </tr>
+	    	  <tr>
+	    <td>&nbsp;</td>
+	  </tr>
+	  <tr>
+            <td>&nbsp;</td><td><input name="submitButton" type="image" title="Register" src="images/register.png" /></td>
+	  </tr>
+	</form>
+        </table>
+      </div>
+    </div>
+  </div>
+</div>
+<div id="footer">
+<div id="footer_content"><a href="premium_dedicated_servers.php">Premium dedicated</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a href="custom_dedicated_servers.php">Custom dedicated</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a href="special.php">Specials</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a href="unmetered.php">Unmetered</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a href="lease-to-own.php">Lease-to-Own</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a href="reseller.php">Reseller</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a href="colocation.php">Colocation</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a href="about.php">About us</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a href="contact.php">Contact</a></div>
+  <div id="copyright">
+  <img src="images/shade.gif" width="940" height="16" alt="shade" />
+  All prices on this website are excluding 21% VAT and based on a payment per 6 months. All offers are subject to our <a href="terms.php">Terms and conditions</a>.<br /><br />
+    <table width="320" border="0" cellspacing="0" cellpadding="0" align="center">
+      <tr>
+        <td width="30"><img src="images/ws_icon.gif" width="28" height="28" alt="WorldStream" /></td>
+      <td width="290">© 2015 WorldStream B.V. All rights reserved. </td>
+	</tr></table>
+    </div>
+</div>
+<script type="text/javascript"> Cufon.now(); </script>
+</body>
+</html>
