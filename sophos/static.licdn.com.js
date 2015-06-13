ddee5e6a7f3a0dcb97d26f88515e06161fa78e5b
Pattern Juggled ie ðørkßöt <pj@cryptostorm.is>
2015-06-13 04:23:30 +0200

    Create static.licdn.com.js
    https://static.licdn.com/

---
+++ sophos/static.licdn.com.js
@@ -0,0 +1,50 @@
+
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
+        "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+  <title>404: Page Not Found</title>
+  <meta http-equiv="content-type" content="text/html; charset=UTF-8">
+  <link rel="stylesheet" type="text/css" href="//www.linkedin.com/css/style.css">
+</head>
+
+<body class="errorpg">
+
+<div id="header">
+  <a href="//www.linkedin.com/home"><img src="//www.linkedin.com/img/logos/logo.gif" width="129" height="36" alt="Linkedin"></a>
+</div>
+
+<div id="main" class="error404">
+  <h1>Page Not Found</h1>
+  <p>The page you requested is no longer available, or cannot be found.</p>
+  <p class="bottom">Please double-check the URL (address) you used, or <a href="mailto:customer_service@linkedin.com">contact us</a> if you feel you have reached this page in error.</p>
+  <p class="top helper"><strong>Click the &#8220;Back&#8221; button on your browser or <a href="//www.linkedin.com/home">go to the home page</a>.</strong></p>
+
+  <br>
+
+  <p><strong>Are you looking for any of these LinkedIn features?</strong></p>
+  <ul>
+    <li><a href="//www.linkedin.com/search">Find People</a></li>
+    <li><a href="//www.linkedin.com/jobs">Find Jobs</a></li>
+    <li><a href="//www.linkedin.com/services">Find Services</a></li>
+    <li><a href="//www.linkedin.com/reconnect?displayCategories=">Reconnect with Colleagues</a></li>
+  </ul>
+
+  <ul>
+    <li><a href="//www.linkedin.com/static?key=outlook_toolbar_download">Outlook Toolbar</a></li>
+    <li><a href="//www.linkedin.com/static?key=firefox_companion_download">Firefox Companion</a></li>
+    <li><a href="//www.linkedin.com/static?key=ie_toolbar_download">Internet Explorer Toolbar</a></li>
+    <li><a href="//www.linkedin.com/static?key=groups_info">LinkedIn for Groups</a></li>
+  </ul>
+
+  <ul>
+    <li><a href="https://www.linkedin.com/secure/settings">My Account</a></li>
+    <li><a href="//www.linkedin.com/profile">My Profile</a></li>
+    <li><a href="//www.linkedin.com/connections">My Connections</a></li>
+    <li><a href="//www.linkedin.com/inviteMany">Add Connections</a></li>
+  </ul>
+  <div class="spacer">&nbsp;</div>
+</div>
+
+</body>
+</html>
